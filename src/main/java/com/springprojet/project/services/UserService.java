e43020579520fb235b787915d5d877460e8d0a52
Lucas Ferreira <ferreiravpl@gmail.com>
2024-02-17 17:17:11 -0300

    Exception Handler

--- src/main/java/com/springprojet/project/services/UserService.java
+++ src/main/java/com/springprojet/project/services/UserService.java
@@ -2,6 +2,7 @@ package com.springprojet.project.services;
 
 import com.springprojet.project.entities.User;
 import com.springprojet.project.repositories.UserRepository;
+import com.springprojet.project.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert (User obj) {
